5864dcf564a98982819194038bd9a6e3d9cc0049
Andrey Furlan <furlan@gettrik.com>
2019-02-09 15:38:01 +0000

    Updates

---
+++ precache-manifest.f7af6361f744aa1d08ee2239eb8233b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c8d0ffc6e080eba2d628",
+    "url": "/new-app/static/js/runtime~main.c8d0ffc6.js"
+  },
+  {
+    "revision": "b21537bf7356127fc357",
+    "url": "/new-app/static/js/main.b21537bf.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/new-app/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "b21537bf7356127fc357",
+    "url": "/new-app/static/css/main.f2ad405a.chunk.css"
+  },
+  {
+    "revision": "d217bd4820092678740360fba73a2404",
+    "url": "/new-app/index.html"
+  }
+];
