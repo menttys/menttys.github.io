3e800e1e8508bb26870a2ca044cd507b606a544c
Andrey Furlan <furlan@gettrik.com>
2019-02-09 14:42:02 +0000

    pushing

---
+++ test/build/precache-manifest.0a62794b75debbcc07b980dba3ac544f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5ad1e219c88e053e0b1e",
+    "url": "/test/static/js/runtime~main.5ad1e219.js"
+  },
+  {
+    "revision": "8e3b63a4d13581a5b20d",
+    "url": "/test/static/js/main.8e3b63a4.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/test/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "8e3b63a4d13581a5b20d",
+    "url": "/test/static/css/main.972310b0.chunk.css"
+  },
+  {
+    "revision": "578823cf715810ad44675105e211809b",
+    "url": "/test/index.html"
+  }
+];
